f54c351fea04ae557126b403da8eb4757da9e5fa
Luiz Freneda <lfreneda@gmail.com>
2016-05-13 12:50:42 -0300

    Verbose logging

--- src/FieldControl.CPlusSync.ConsoleApp/Program.cs
+++ src/FieldControl.CPlusSync.ConsoleApp/Program.cs
@@ -19,6 +19,7 @@ namespace FieldControl.CPlusSync.ConsoleApp
             if (args.Length > 1)
             {
                 bool verboseLog = Convert.ToBoolean(args[1]);
+                FileLog.Verbose = true;
             }
 
             FileLog.WriteLine(string.Format("Data para sincronizar: {0}", syncDate.ToShortDateString()));
